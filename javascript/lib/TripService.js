fd12d0e15c05921e65200928a4db360a42b95e81
Juan Carlos Serrano <juancsch@gmail.com>
2018-09-24 10:39:49 +0200

    Extract guard clauses to method

--- javascript/lib/TripService.js
+++ javascript/lib/TripService.js
@@ -5,28 +5,31 @@ module.exports = class TripService {
 
 	getTripsByUser (user) {
 
-		let tripList = []
 		let loggedUser = this.getLoggedUser()
+		this._validate(loggedUser)
+
+		let tripList = []
 		let isFriend = false
 
-		if (loggedUser != null) {
-
-			let friends = user.getFriends()
-
-			for (let i = 0; i < friends.length; i++) {
-				let friend = friends[i]
-				if (friend === loggedUser) {
-					isFriend = true
-					break
-				}
+		let friends = user.getFriends()
+		for (let i = 0; i < friends.length; i++) {
+			let friend = friends[i]
+			if (friend === loggedUser) {
+				isFriend = true
+				break
 			}
+		}
 
-			if (isFriend) {
-				tripList = this.findTripsBy(user)
-			}
+		if (isFriend) {
+			tripList = this.findTripsBy(user)
 
-			return tripList
-		} else {
+		}
+
+		return tripList
+	}
+
+	_validate(loggedUser) {
+		if (loggedUser == null) {
 			throw new Error('User not logged in.')
 		}
 	}
